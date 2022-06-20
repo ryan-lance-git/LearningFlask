f29726417e3e2a19621aa09a413f2d9e838e17d9
Ryan <ryan.lance.3141@gmail.com>
2022-06-19 17:05:20 -0700

    Added navigation bar and "Contact Us" page

--- FlaskWebProject1/HelloFlask/views.py
+++ FlaskWebProject1/HelloFlask/views.py
@@ -1,8 +1,34 @@
-
-from flask import Flask
+from datetime import datetime
+from flask import render_template
 from HelloFlask import app
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello Flask!"
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    return render_template(
+        "index.html",
+        message = "Hello, Flask ",
+        content = formatted_now)
+
+
+@app.route('/api/data')
+def get_data():
+    return app.send_static_file('data.json')
+
+@app.route('/about')
+def about():
+    return render_template(
+        "about.html",
+        title = "About HelloFlask", 
+        content = "Example app page for Flask. ")
+
+@app.route('/contact_us')
+def contact_us():
+    return render_template(
+        "contact_us.html",
+        title = "Contact the developers",
+        content1 = "Ryan Lance",
+        content2 = "Email: ryan.lance.3141@gmail.com")
